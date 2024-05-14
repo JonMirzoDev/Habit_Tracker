677cc4c53486f3620c4280500708d8989d911538
JonMirzoDev <islomjon.swe@gmail.com>
2024-05-14 11:12:51 +0500

    habit databse created using isar flutter database

---
+++ lib/database/habit_database.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:habit_tracker/models/app_settings.dart';
+import 'package:habit_tracker/models/habit.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
+
+class HabitDatabase extends ChangeNotifier {
+  static late Isar isar;
+
+  static Future<void> initialize() async {
+    final dir = await getApplicationDocumentsDirectory();
+    isar =
+        await Isar.open([HabitSchema, AppSettingsSchema], directory: dir.path);
+  }
+
+  Future<void> saveFirstLaunchDate() async {
+    final existingSettings = await isar.appSettings.where().findFirst();
+
+    if (existingSettings == null) {
+      final settings = AppSettings()..firstLaunchDate = DateTime.now();
+      await isar.writeTxn(() => isar.appSettings.put(settings));
+    }
+  }
+
+  Future<DateTime?> getFirstLaunchDate() async {
+    final settings = await isar.appSettings.where().findFirst();
+    return settings?.firstLaunchDate;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:habit_tracker/database/habit_database.dart';
 import 'package:habit_tracker/pages/home_page.dart';
 import 'package:habit_tracker/theme/theme_provider.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await HabitDatabase.initialize();
+  await HabitDatabase().saveFirstLaunchDate();
+
   runApp(ChangeNotifierProvider(
     create: (context) => ThemeProvider(),
     child: const MyApp(),
